e249a30bac352083b97653a7f56ff37857077795
qichunren <whyruby@gmail.com>
2012-06-13 00:05:06 +0800

    fixed test issue

--- test/factories/g_users.rb
+++ test/factories/g_users.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :user, :class => GUser do
     sequence(:name){|n| "name#{n}" }
     sequence(:email){|n| "email#{n}@codecampo.com" }
+    sequence(:oauth_access_token) {|n| "oauth_access_#{n}_token" }
+    sequence(:avatar_url) {|n| "https://github.com/#{n}.png" }
+    followers_count 0
+    following_count 0
+    public_repos_count 0
+    public_gists_count 0
+    profile { FactoryGirl.build(:profile) }
   end
 end
--- test/factories/profiles.rb
+++ test/factories/profiles.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :profile do
+    name "github_user"
+    url "https://github.com/qichunren"
+    description "balabala"
   end
 end
